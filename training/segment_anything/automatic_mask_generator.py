e152da2fc86c31b86c62262dfcb242bb92c90e10
TouHouE <hsu.william9@gmail.com>
2024-06-15 17:28:11 +0800

    Change I.4

--- training/segment_anything/automatic_mask_generator.py
+++ training/segment_anything/automatic_mask_generator.py
@@ -10,9 +10,9 @@ import numpy as np
 import torch
 from torchvision.ops.boxes import batched_nms, box_area  # type: ignore
 
-from .modeling import Sam
-from .predictor import SamPredictor
-from .utils.amg import (
+from training.segment_anything.modeling import Sam
+from training.segment_anything.predictor import SamPredictor
+from training.segment_anything.utils.amg import (
     MaskData,
     area_from_rle,
     batch_iterator,
