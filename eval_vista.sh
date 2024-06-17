3427c342fde633448314e1d8d5bfad47329b329b
TouHouE <hsu.william9@gmail.com>
2024-06-17 21:15:03 +0800

    add scripts

---
+++ eval_vista.sh
@@ -0,0 +1,7 @@
+export ckpt_path="/mnt/src/VISTA_workspace/runs/vista-h-b1/06-02_23-40-25/model_best.pt"
+export ckpt_path_1="/mnt/src/VISTA_workspace/runs/vista-h-b1/06-05_12-30-51/model_best.pt"
+export ckpt_path_large="/mnt/src/VISTA_workspace/runs/vista-l-b1/06-12_14-42-37/model_best.pt"
+export ckpt_4="/mnt/src/VISTA_workspace/runs/vista-h-b1/05-30_03-47-23/model_best.pt"
+python /mnt/src/VISTA_workspace/VISTAForCT/inference/eval_vista.py --ckpt_path=$ckpt_path --vit_type=vit_h --output_folder=/mnt/src/VISTA_workspace/pred_train --image_size=512 \
+	--json_form=/mnt/src/data/vista_table.json --image_folder=/mnt/src/data \
+	--patch_embed_3d --debug=50 --b_min=0
---
+++ normal_train.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+
+# export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+# export MASTER_PORT=$(( RANDOM % (50000 - 30000 + 1 ) + 30000 ))
+export GPUS_PER_NODE=$SLURM_GPUS_PER_NODE
+export NNODES=$SLURM_NNODES
+# export NUM_PROCESSES=$(expr $NNODES \* $GPUS_PER_NODE)
+
+export WORLD_SIZE=$SLURM_NNODES
+
+export HSU_HOME="/mnt/home/l159753807"
+export SHARE_HOME="/mnt/share/NTU_Heart_CT"
+export CONTAINER_NAME="$HSU_HOME/workspace/dimage/nemo_monai.sqsh"
+
+export DATA_MNT="${SHARE_HOME}/data:/workspace/data"
+# export MODEL_MNT="$HSU_HOME/workspace/model-weight:/workspace/model-weight"
+export PROJ_MNT="$SHARE_HOME/VISTA_workspace/CTVISTA/training:/workspace"
+
+export CONTAINER_MOUNT="$DATA_MNT,$PROJ_MNT"
+export MAIN1="main_2pt5d.py"
+export MAIN2="second_main.py"
+export DIST="--distributed"
+export vit_size='h'
+export batch_size=16
+export time=$(date +%m-%d_%H-%M-%S)
+export reuse_ckpt="/mnt/VISTA_workspace/runs/vista-h-b1/05-30_03-47-23"
+export torchrun="torchrun --nproc-per-node=1 --nnodes=1 --master-port=11234"
+
+export PYTHON_CMD="
+	python ./training/main_2pt5d.py --nc=11 --project=Vista \
+--max_epochs 5000 --val_every 1 --optim_lr 0.00005 \
+--num_patch 160 --num_prompt 32 \
+--json_list /mnt/src/data/vista_table.json \
+--data_dir /mnt/src/data --use_normal_dataset \
+--roi_z_iter 27 --save_checkpoint \
+--sam_base_model vit_${vit_size} \
+--logdir /mnt/src/VISTA_workspace/ckpt/vista-${vit_size}-b$batch_size/$time --point_prompt --label_prompt --seed 114514 \
+--iterative_training_warm_up_epoch 50 --batch_size=$batch_size \
+--label_prompt_warm_up_epoch 25 --patch_embed_3d --fold=0 --name=vista-${vit_size}-b$batch_size-${time}_no_cache_full --sam_image_size=512 \
+--wandb --workers=16 
+"
+
+#echo "MASTER_ADDR: $MASTER_ADDR"
+#echo "MASTER_PORT: $MASTER_PORT"
+
+$PYTHON_CMD
+
